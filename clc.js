9baf9322cbad95edd828d1241f3cc9936fc7f654
felix1feliz <sonicrobogigante@gmail.com>
2024-10-15 03:43:02 -0300

    Added missing ';'

--- clc.js
+++ clc.js
@@ -218,7 +218,7 @@ class Tokenizer {
 				"OUT":  new Token("OUT" ),
 				"INP":  new Token("INP" ),
 				"OUT":  new Token("OUT" ),
-			}
+			};
 
 			if(this._isAlnum(this._code.charAt(i))) {
 				let acc = "";
